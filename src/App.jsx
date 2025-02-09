b21136de36bf4ede2307bf8c9ad48defa900b342
Kazi Forkan Uddin <rakib.fin2404509@gmail.com>
2025-02-10 00:50:10 +0200

    Refactor Content component and create Part component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-// Header Component
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
+// Part Component
+const Part = ({ part, exercises }) => {
+  return <p>{part} {exercises}</p>;
 }
 
 // Content Component
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
     <div>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
-  )
+  );
 }
 
 // Total Component
 const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
 }
 
 // App Component (Main Component)
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
 
   return (
     <div>
@@ -39,7 +39,7 @@ const App = () => {
       />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
